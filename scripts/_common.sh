0fb155386bc4c120fae69d1d1102291b8929eb3a
Éric Gaspar <46165813+ericgaspar@users.noreply.github.com>
2024-06-16 11:17:46 +0200

    Update _common.sh

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -7,7 +7,7 @@
 #=================================================
 
 # nodejs version
-nodejs_version=18
+nodejs_version=20
 version_commit=fcf9fcea52d1e09d689ed6998d729aa8d08be604
 
 #=================================================
