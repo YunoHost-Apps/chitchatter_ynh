42b9f63b0ae89f82424dd100f247831f75186a13
eric_G <46165813+ericgaspar@users.noreply.github.com>
2025-12-29 16:30:27 +0100

    cleaning

--- scripts/_common.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-#=================================================
-# COMMON VARIABLES AND CUSTOM HELPERS
-#=================================================
-
-nodejs_version=20
-version_commit=66ffd169485caf7fdf2792b025d53964ba474968
