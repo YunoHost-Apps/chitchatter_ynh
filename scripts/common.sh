d5f390c02fcc48453f013e4fa3cd9337b4830979
eric_G <46165813+ericgaspar@users.noreply.github.com>
2025-12-29 16:44:02 +0100

    cleaning

--- scripts/common.sh
+++ scripts/common.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
 
-#=================================================
-# COMMON VARIABLES AND CUSTOM HELPERS
-#=================================================
-
+version_commit=11542db41f0e1fbf1dfea13ec07b010fc1cfae00
